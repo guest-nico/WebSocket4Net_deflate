378347ba69b09eeac0c17809a5ea1679fd94841b
guest-nico <kogakogaga@yahoo.co.jp>
2018-12-02 04:35:28 +0900

    first

---
+++ WebSocket4Net/MessageReceivedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSocket4Net
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+    	public MessageReceivedEventArgs(string message, byte[] data)
+        {
+            Message = message;
+            Data = data;
+        }
+
+        public string Message { get; private set; }
+        public byte[] Data { get; private set; }
+    }
+}
